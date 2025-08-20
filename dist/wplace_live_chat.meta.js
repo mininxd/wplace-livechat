92a1240160f056527cf68208cf1a89fd64ba136c
Minin <70429604+mininxd@users.noreply.github.com>
2025-08-20 18:36:32 +0700

    Delete dist directory

--- dist/wplace_live_chat.meta.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name     wplace live chat
-// @version  1.0.0
-// @match    http://localhost:3000
-// @match    http://localhost:5173
-// @match    https://wplace.live
-// ==/UserScript==
