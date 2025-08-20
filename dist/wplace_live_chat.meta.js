b388b1ca49bdb14a9e53ffd0415877102abbe209
root <mininsna@gmail.com>
2025-08-20 12:02:37 +0700

    test

---
+++ dist/wplace_live_chat.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name     wplace live chat
+// @version  1.0.0
+// @match    http://localhost:3000
+// @match    http://localhost:5173
+// @match    https://wplace.live
+// ==/UserScript==
