0ff42ad77dd497ea08bac8d8749e3840fc83a657
Minin <70429604+mininxd@users.noreply.github.com>
2025-08-20 22:15:16 +0700

    Delete wplace_livechat.bookmark.js

--- wplace_livechat.bookmark.js
+++
@@ -1,9 +0,0 @@
-javascript:(function(){
-    const repoUrl = 'YOUR_GITHUB_REPO_URL_HERE'; // e.g., 'https://github.com/user/repo'
-    const scriptUrl = repoUrl.replace('github.com', 'raw.githubusercontent.com') + '/main/dist/script.build.user.js';
-
-    fetch(scriptUrl)
-        .then(r => r.text())
-        .then(code => eval(code))
-        .catch(err => console.error('Error loading Wplace Live Chat script:', err));
-})();
